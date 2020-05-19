970b518bf4bd2577568fcf4137775e878902ac72
Guo Xiang Tan <gxtan1990@gmail.com>
2020-05-19 13:32:00 +0800

    Resolving minor styling issue.

--- lib/rails_failover.rb
+++ lib/rails_failover.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
+
 require "rails_failover/version"
 
 module RailsFailover
   class Error < StandardError; end
-  # Your code goes here...
 end
--- rails_failover.gemspec
+++ rails_failover.gemspec
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative 'lib/rails_failover/version'
 
 Gem::Specification.new do |spec|
