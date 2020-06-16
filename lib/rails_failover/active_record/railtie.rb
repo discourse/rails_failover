9fdb5031f679c3419b258be349e347701daa7479
Guo Xiang Tan <gxtan1990@gmail.com>
2020-06-16 10:45:49 +0800

    FIX: Don't set up gem unless database config is valid.

--- lib/rails_failover/active_record/railtie.rb
+++ lib/rails_failover/active_record/railtie.rb
@@ -3,40 +3,49 @@
 module RailsFailover
   module ActiveRecord
     class Railtie < ::Rails::Railtie
-      initializer "rails_failover.init", after: "active_record.initialize_database" do
-        ::ActiveSupport.on_load(:active_record) do
-          Handler.instance
+      initializer "rails_failover.init", after: "active_record.initialize_database" do |app|
+        config = ::ActiveRecord::Base.connection_config
+        app.config.active_record_rails_failover = false
 
-          # We are doing this manually for now since we're awaiting Rails 6.1 to be released which will
-          # have more stable ActiveRecord APIs for handling multiple databases with different roles.
-          ::ActiveRecord::Base.connection_handlers[::ActiveRecord::Base.reading_role] =
-            ::ActiveRecord::ConnectionAdapters::ConnectionHandler.new
+        if !!(config[:replica_host] && config[:replica_port])
+          app.config.active_record_rails_failover = true
 
-          ::ActiveRecord::Base.connection_handlers[::ActiveRecord::Base.writing_role].connection_pools.each do |connection_pool|
-            RailsFailover::ActiveRecord.establish_reading_connection(
-              ::ActiveRecord::Base.connection_handlers[::ActiveRecord::Base.reading_role],
-              connection_pool.spec
-            )
-          end
+          ::ActiveSupport.on_load(:active_record) do
+            Handler.instance
 
-          begin
-            ::ActiveRecord::Base.connection
-          rescue ::ActiveRecord::NoDatabaseError
-            # Do nothing since database hasn't been created
-          rescue ::PG::Error => e
-            Handler.instance.verify_primary(::ActiveRecord::Base.writing_role)
-            ::ActiveRecord::Base.connection_handler = ::ActiveRecord::Base.lookup_connection_handler(:reading)
+            # We are doing this manually for now since we're awaiting Rails 6.1 to be released which will
+            # have more stable ActiveRecord APIs for handling multiple databases with different roles.
+            ::ActiveRecord::Base.connection_handlers[::ActiveRecord::Base.reading_role] =
+              ::ActiveRecord::ConnectionAdapters::ConnectionHandler.new
+
+            ::ActiveRecord::Base.connection_handlers[::ActiveRecord::Base.writing_role].connection_pools.each do |connection_pool|
+              RailsFailover::ActiveRecord.establish_reading_connection(
+                ::ActiveRecord::Base.connection_handlers[::ActiveRecord::Base.reading_role],
+                connection_pool.spec
+              )
+            end
+
+            begin
+              ::ActiveRecord::Base.connection
+            rescue ::ActiveRecord::NoDatabaseError
+              # Do nothing since database hasn't been created
+            rescue ::PG::Error => e
+              Handler.instance.verify_primary(::ActiveRecord::Base.writing_role)
+              ::ActiveRecord::Base.connection_handler = ::ActiveRecord::Base.lookup_connection_handler(:reading)
+            end
           end
         end
       end
 
       initializer "rails_failover.insert_middleware" do |app|
-        ActionDispatch::DebugExceptions.register_interceptor do |request, exception|
-          RailsFailover::ActiveRecord::Interceptor.handle(request, exception)
-        end
+        if app.config.active_record_rails_failover
+          ActionDispatch::DebugExceptions.register_interceptor do |request, exception|
+            RailsFailover::ActiveRecord::Interceptor.handle(request, exception)
+          end
 
-        if !skip_middleware?(app.config)
-          app.middleware.unshift(::RailsFailover::ActiveRecord::Middleware)
+          if !skip_middleware?(app.config)
+            app.middleware.unshift(::RailsFailover::ActiveRecord::Middleware)
+          end
         end
       end
 
